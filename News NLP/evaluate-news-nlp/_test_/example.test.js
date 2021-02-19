760cad02f816b6217022602a7d3790edf3bbee7d
Marjean D Mayo-baker <marjean_m@hotmail.com>
2021-02-18 22:52:26 -0500

    final commit of changes

--- News NLP/evaluate-news-nlp/_test_/example.test.js
+++
@@ -1,9 +0,0 @@
-import { sum } from '../src/client/js/example'
-test('adds 12 + 14 to equal 26',() =>{
-    expect (sum(12,14)).toBe(26);
-})
-import { validateStrings } from'../src/client/js/example'
-test('validate strings', () => {
-    expect(validateStrings("")).toBe(false);
-    expect(validateStrings("The wonderful world of Disney")).toBe(true);
-  });
